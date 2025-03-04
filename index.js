6aa0fd50c669b95dbf9d74d905526ecfa822f55f
Ali Sheikh Zubeir Noor <ali.noor1@student.moringaschool.com>
2025-03-04 07:01:34 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+    let result;
+    if (distance <= 400) {
+       result = "This one is on me!";
+       return result;
+    } else if (distance > 400 && distance <= 2000) {
+      result = "That will be twenty bucks.";
+      return result;
+    } else if (distance > 2000 && distance <= 2500) {
+      result = "I will gladly take your thirty bucks.";
+      return result;
+    } else {
+      result = "No can do.";
+      return result;
+    }
+    
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(location){
+  return location === "NYC" ? ("Ok, sounds good."): ("No go.");
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money) {
+    case "generous":
+      return "Thank you so much.";
+      return result;
+    case "not as generous":
+      return "Thank you.";
+      return result;
+    default:
+      return "Bye.";
+      return result;
+  }
 }
